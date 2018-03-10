ef44580675a343b44f3c69e990f1907377c98f79
Mikhail Demchenko <dev.echo.mike@gmail.com>
2018-03-10 13:21:42 +0300

    General propose macro sepparated.

---
+++ Common++/header/MacroUtils.h
@@ -0,0 +1,8 @@
+#ifndef PCAPPP_MACRO_UTILS_H
+#define PCAPPP_MACRO_UTILS_H
+/**
+ * Special macro that can select between two functional-macro definitions based on count of arguments
+ */
+#define PCAPPP_GET_MACRO_2(_1,_2,NAME,...) NAME
+
+#endif /* PCAPPP_MACRO_UTILS_H */
